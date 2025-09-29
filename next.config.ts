3f6654fa18199590169ab8ed06836b184fdf2a64
Arnold Choy <arnold@mlmprotec.com>
2025-09-29 02:00:59 -0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/next',
-  assetPrefix: '/next',
   trailingSlash: true,
   output: 'standalone',
   eslint: {
